e3a09cee670a54ccd7ae54b39054f0dea6b177b2
marcaotrabalhador <145072746+marcaotrabalhador@users.noreply.github.com>
2023-09-14 20:17:13 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite um número: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int contador = 0;
+
+        while (contador < n)
+        {
+            Console.WriteLine(nome);
+            contador++;
+        }
+    }
+}
